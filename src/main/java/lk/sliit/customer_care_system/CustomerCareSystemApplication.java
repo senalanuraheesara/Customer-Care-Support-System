530ad28c60e87ca3bfeee1c44488fcd05c9f48dc
Senal Anuraheesaera <senalanuraheesara@gmail.com>
2026-02-09 23:55:09 +0530

    Initial Commit

---
+++ src/main/java/lk/sliit/customer_care_system/CustomerCareSystemApplication.java
@@ -0,0 +1,13 @@
+package lk.sliit.customer_care_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerCareSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerCareSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lk/sliit/customer_care_system/CustomerCareSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.sliit.customer_care_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerCareSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
