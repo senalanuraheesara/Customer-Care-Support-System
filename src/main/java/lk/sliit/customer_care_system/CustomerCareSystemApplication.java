e9e67a94680aa75575aa7d4867c419f4609ac1fe
Senal Anuraheesaera <senalanuraheesara@gmail.com>
2026-02-10 10:45:19 +0530

    Update CustomerCareSystemApplication.java

--- src/main/java/lk/sliit/customer_care_system/CustomerCareSystemApplication.java
+++ src/main/java/lk/sliit/customer_care_system/CustomerCareSystemApplication.java
@@ -2,8 +2,10 @@ package lk.sliit.customer_care_system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class CustomerCareSystemApplication {
 
     public static void main(String[] args) {
